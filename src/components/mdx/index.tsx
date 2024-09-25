e9f19fd3114006aac81582a7ffcf3838b1274cce
DustVoyager <ohjamdol@gmail.com>
2024-09-25 22:00:47 +0900

    vercel 배포 오류 수정

--- src/components/mdx/index.tsx
+++ src/components/mdx/index.tsx
@@ -2,10 +2,11 @@ import { Callout } from './Callout';
 import { Image } from './Image';
 import { ExternalLink } from './Link';
 import { MDXComponents } from 'mdx/types';
+import { ComponentProps } from 'react';
 
 export const MdxComponents: MDXComponents = {
-  a: ExternalLink as any,
-  img: Image as any,
+  a: ExternalLink as React.ComponentType<ComponentProps<'a'>>,
+  img: Image as React.ComponentType<ComponentProps<'img'>>,
   blockquote: Callout,
   Callout,
 };
