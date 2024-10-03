29e12b806342ecb51652c51d3565242289571d64
DustVoyager <ohjamdol@gmail.com>
2024-10-03 22:00:55 +0900

    next.config.js 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // public 폴더 안의 모든 파일에 대해 Cache-Control 헤더 적용
+        source: "/:all*(png|jpg|jpeg|svg|gif)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, max-age=0, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
